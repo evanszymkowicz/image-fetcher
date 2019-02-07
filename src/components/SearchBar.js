5e1ded6cbeecce7acdad0eaca2ce0e6e569b445b
evanszymkowicz <evan.szymkowicz@evanwolf.co>
2019-02-06 21:31:29 -0500

    Search event handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,20 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: '' };
+
   render() {
     return (
-      <div>
-        <form>
-          <input type="text" />
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+          <label>Search for an image!</label>
+          <input
+            type="text"
+            value={this.state.term}
+            onChange={(e) => this.setState({ term: e.target.value })} /* don't use an () with an event handler in state*/
+            />
+          </div>
         </form>
       </div>
     )
@@ -13,3 +22,10 @@ class SearchBar extends React.Component {
 }
 
 export default SearchBar;
+
+/*
+This has been refactored to be a "controlled component."
+A callback is invoked as soon as the user types in an input. The result is
+pulled from the event object and then updates the state, pulled from e.target.value
+and rerenders automatically.
+*/
