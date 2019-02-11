b9acc2d60a130a8c070478eaf6012e03e1baf245
evanszymkowicz <evan.szymkowicz@evanwolf.co>
2019-02-11 17:19:03 -0500

    api component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
   /* images property.
@@ -8,23 +9,20 @@ class App extends React.Component {
    Objects ([]) have the built in .map function, but the value `null` does not. */
   state = { images: [] };
 
-  onSearchSubmit = async (term) => {
-    const response = await axios.get('https://api.unsplash.com/search/photos', {
+  onSearchSubmit = async term => {
+    const response = await unsplash.get('search/photos', {
         //params specifiies which query terms are added on to the GET request
-        params: { query: term },
-        headers: {
-          Authorization: 'Client-ID d92d7481830508f78be383ff84156b0ba9d6f834d29cdd67f4541db18b27af32'
-          //referred to as access key in Upsplash docs
-        }
+        params: { query: term }
       });
+
       this.setState({ images: response.data.results });
-    }
+  };
 
   render() {
     return (
       <div className= "ui container" style={{ marginTop: '10px'}}>
           <SearchBar onSubmit={this.onSearchSubmit} />
-          Found: {this.state.images.length} images
+          <ImageList images={this.state.images} />
       </div>
       /*This is another reason to use [] instead of null. Cannot render
         this.state.images.length.null. BUT: resturn array results cause JS, yo
